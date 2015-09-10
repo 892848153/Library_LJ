f1ee4c27a316beeac0e8ab6fe52286d578074201
刘杰 <892848153@qq.com>
2015-09-10 23:25:11 +0800

    删除项目
    删除项目

--- Library_LJ/src/com/lj/library/fragment/BackHandlerInterface.java
+++
@@ -1,9 +0,0 @@
-package com.lj.library.fragment;
-
-/**
- * Created by liujie_gyh on 15/9/4.
- */
-public interface BackHandlerInterface {
-
-     void setSelectedFragment(BaseFragment selectedFragment);
-}
