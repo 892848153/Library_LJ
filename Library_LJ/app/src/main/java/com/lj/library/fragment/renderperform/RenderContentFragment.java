d8b5e048564557656edbd28baeacd5fc606aeee0
刘杰 <892848153@qq.com>
2016-03-01 21:57:29 +0800

    重构
    重构

--- Library_LJ/app/src/main/java/com/lj/library/fragment/renderperform/RenderContentFragment.java
+++ Library_LJ/app/src/main/java/com/lj/library/fragment/renderperform/RenderContentFragment.java
@@ -1,6 +1,5 @@
 package com.lj.library.fragment.renderperform;
 
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 
@@ -12,15 +11,15 @@ import com.lj.library.fragment.BaseFragment;
 public class RenderContentFragment extends BaseFragment {
     @Override
     protected View onCreateView(LayoutInflater inflater) {
-        int layoutId = getLayoutId(getArguments());
+        int layoutId = getLayoutId();
         View view = layoutId != -1 ? inflater.inflate(layoutId, null) : null;
         return view;
     }
 
-    private int getLayoutId(Bundle args) {
+    private int getLayoutId() {
         int layoutId = -1;
-        if (args != null) {
-            layoutId = args.getInt("layoutId", -1);
+        if (getArguments() != null) {
+            layoutId = getArguments().getInt("layoutId", -1);
         }
         return layoutId;
     }
