0d431b1ee59244142bba990bb27451699d06b2ea
刘杰 <892848153@qq.com>
2016-04-18 21:50:25 +0800

    okhttp增加缓存
    okhttp增加缓存

--- Library_LJ/app/src/main/java/com/lj/library/http/okhttp/OkHttpManager.java
+++ Library_LJ/app/src/main/java/com/lj/library/http/okhttp/OkHttpManager.java
@@ -1,5 +1,10 @@
 package com.lj.library.http.okhttp;
 
+import com.lj.library.util.SDCardUtil;
+
+import java.io.File;
+
+import okhttp3.Cache;
 import okhttp3.OkHttpClient;
 
 /**
@@ -13,13 +18,14 @@ public enum OkHttpManager {
     private static OkHttpClient sOkHttpClient;
 
     private static final long HTTP_RESPONSE_DISK_CACHE_MAX_SIZE = 10 * 1024 * 1024;
+    private static final String OKHTTP_CACHE_DIR = "okhttpCache";
 
     static {
         // Interceptor有两种,详情见https://github.com/square/okhttp/wiki/Interceptors
         // 开启缓存响应数据功能  http://www.devtf.cn/?p=1264
-//        final File baseDir = new File();
+        final File baseDir = new File(SDCardUtil.getSDPath(), OKHTTP_CACHE_DIR);
         sOkHttpClient = new OkHttpClient.Builder()
-//                .cache(new Cache(baseDir, HTTP_RESPONSE_DISK_CACHE_MAX_SIZE))
+                .cache(new Cache(baseDir, HTTP_RESPONSE_DISK_CACHE_MAX_SIZE))
                 .addInterceptor(CommonHeadersInterceptor.getInstance())
                 .addInterceptor(LoggingInterceptor.getInstance()).build();
     }
