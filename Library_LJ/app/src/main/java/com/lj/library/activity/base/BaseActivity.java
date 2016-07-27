f5d7d57215a2c60edc012673b1f74de710e8e331
liujie <892848153@qq.com>
2016-07-27 17:59:36 +0800

    增加Translucent app bar的实现
    对android4.4及以上的版本实现Translucent app bar的功能，
    4.4系统的情况下，弄一个跟状态栏一样高的View，将其顶到状态栏下面
    以实现修改状态栏背景的目的。
    5.0及以上系统的情况下，直接修改状态栏的背景色。

--- Library_LJ/app/src/main/java/com/lj/library/activity/base/BaseActivity.java
+++ Library_LJ/app/src/main/java/com/lj/library/activity/base/BaseActivity.java
@@ -1,13 +1,19 @@
 package com.lj.library.activity.base;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 
+import com.lj.library.R;
 import com.lj.library.application.MyApplication;
 import com.lj.library.fragment.BackHandlerInterface;
 import com.lj.library.fragment.BaseFragment;
 import com.lj.library.util.RxBus;
+import com.lj.library.util.UIUtils;
 
 import butterknife.ButterKnife;
 
@@ -17,7 +23,7 @@ import butterknife.ButterKnife;
  * @author jie.liu
  * @time 2015年3月6日 上午10:55:47
  */
-public abstract class BaseActivity extends FragmentActivity implements BackHandlerInterface {
+public abstract class BaseActivity extends AppCompatActivity implements BackHandlerInterface {
 
     protected Activity mContext;
 
@@ -35,6 +41,27 @@ public abstract class BaseActivity extends FragmentActivity implements BackHandl
             setContentView(layoutId);
             ButterKnife.bind(this);
             initComp(savedInstanceState);
+        }
+
+        translucentStatusBar();
+    }
+
+    /**
+     * 根据系统版本调整属性ui，实现Translucent app bar
+     */
+    private void translucentStatusBar() {
+        View parentView =((ViewGroup) findViewById(Window.ID_ANDROID_CONTENT)).getChildAt(0);
+        View statusBarHolder = findViewById(R.id.status_bar_holder_view);
+        // 5.0系统开始可以更改状态栏的颜色，所以不需要状态栏底块
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            statusBarHolder.getLayoutParams().height = 0;
+            parentView.setFitsSystemWindows(true);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // 4.4系统开始支持布局延伸到状态栏底下，不支持更改状态栏颜色，所以需要一个状态栏底块
+            statusBarHolder.getLayoutParams().height = UIUtils.getStatusBarHeight(this);
+            parentView.setFitsSystemWindows(false);
+        } else {
+            statusBarHolder.getLayoutParams().height = 0;
         }
     }
 
