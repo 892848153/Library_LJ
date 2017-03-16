13cd6c9d667954912f3501ad85e177fd7bb91a9b
刘杰 <892848153@qq.com>
2017-03-16 17:46:47 +0800

    优化内存分配
    优化内存分配

--- app/src/main/java/com/lj/library/widget/AnimatorView.java
+++ app/src/main/java/com/lj/library/widget/AnimatorView.java
@@ -95,14 +95,17 @@ public class AnimatorView extends View {
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     private class PointEvaluator implements TypeEvaluator {
 
+        private Point mPoint = new Point(0, 0);
+
         @Override
         public Object evaluate(float fraction, Object startValue, Object endValue) {
             Point startPoint = (Point) startValue;
             Point endPoint = (Point) endValue;
             float x = startPoint.getX() + fraction * (endPoint.getX() - startPoint.getX());
             float y = startPoint.getY() + fraction * (endPoint.getY() - startPoint.getY());
-            Point point = new Point(x, y);
-            return point;
+            mPoint.setX(x);
+            mPoint.setY(y);
+            return mPoint;
         }
     }
 
@@ -114,6 +117,8 @@ public class AnimatorView extends View {
         private int mCurrentGreen = -1;
 
         private int mCurrentBlue = -1;
+
+        private StringBuilder mColorHexValueSb = new StringBuilder(7);
 
         @Override
         public Object evaluate(float fraction, Object startValue, Object endValue) {
@@ -141,19 +146,18 @@ public class AnimatorView extends View {
             int blueDiff = Math.abs(startBlue - endBlue);
             int colorDiff = redDiff + greenDiff + blueDiff;
             if (mCurrentRed != endRed) {
-                mCurrentRed = getCurrentColor(startRed, endRed, colorDiff, 0,
-                        fraction);
+                mCurrentRed = getCurrentColor(startRed, endRed, colorDiff, 0, fraction);
             } else if (mCurrentGreen != endGreen) {
-                mCurrentGreen = getCurrentColor(startGreen, endGreen, colorDiff,
-                        redDiff, fraction);
+                mCurrentGreen = getCurrentColor(startGreen, endGreen, colorDiff, redDiff, fraction);
             } else if (mCurrentBlue != endBlue) {
-                mCurrentBlue = getCurrentColor(startBlue, endBlue, colorDiff,
-                        redDiff + greenDiff, fraction);
+                mCurrentBlue = getCurrentColor(startBlue, endBlue, colorDiff, redDiff + greenDiff, fraction);
             }
             // 将计算出的当前颜色的值组装返回
-            String currentColor = "#" + getHexString(mCurrentRed)
-                    + getHexString(mCurrentGreen) + getHexString(mCurrentBlue);
-            return currentColor;
+            mColorHexValueSb.delete(0, mColorHexValueSb.length()).append('#');
+            appendColorHexString(mCurrentRed, mColorHexValueSb);
+            appendColorHexString(mCurrentGreen, mColorHexValueSb);
+            appendColorHexString(mCurrentBlue, mColorHexValueSb);
+            return mColorHexValueSb.toString();
         }
 
         /**
@@ -179,12 +183,12 @@ public class AnimatorView extends View {
         /**
          * 将10进制颜色值转换成16进制。
          */
-        private String getHexString(int value) {
+        private void appendColorHexString(int value, StringBuilder sb) {
             String hexString = Integer.toHexString(value);
             if (hexString.length() == 1) {
-                hexString = "0" + hexString;
+                sb.append('0');
             }
-            return hexString;
+            sb.append(hexString);
         }
 
     }
