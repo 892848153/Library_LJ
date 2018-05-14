caa6e39f8ea6c2141e9763f5d66a2b019fb726ff
Liujie <892848153@qq.com>
2018-05-14 18:01:47 +0800

    增加自定义控件的参照

--- app/src/main/java/com/lj/library/activity/base/BaseViewModel.java
+++ app/src/main/java/com/lj/library/activity/base/BaseViewModel.java
@@ -10,8 +10,6 @@ public abstract class BaseViewModel implements IBaseViewModel {
 
     protected Activity mContext;
 
-    public String text;
-
     public BaseViewModel(final Activity context) {
         mContext = context;
     }
--- app/src/main/java/com/lj/library/widget/CustomView.java
+++ app/src/main/java/com/lj/library/widget/CustomView.java
@@ -2,16 +2,21 @@ package com.lj.library.widget;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.ViewGroup;
 
 import com.lj.library.R;
 import com.lj.library.util.Logger;
 
 /**
+ * 自定义控件的参照
  * Created by liujie_gyh on 2018/5/13.
+ * @author LJ.Liu
  */
 public class CustomView extends ViewGroup {
 
@@ -28,8 +33,17 @@ public class CustomView extends ViewGroup {
     private int mAttrReference;
 
     private String mAttrString;
+    private String mAttrAndroidText;
 
     private int mAttrEnum;
+
+    private Paint mPaint;
+
+    private int[] mAttrs = {R.attr.global_attr_one, R.attr.global_attr_two};
+    public static final int ATTR_INDEX_ONE = 0;
+    public static final int ATTR_INDEX_TWO = 1;
+
+
 
 
     public CustomView(Context context) {
@@ -80,8 +94,10 @@ public class CustomView extends ViewGroup {
 //                    typedArray.getFraction(R.styleable.CustomView_custom_fraction, )
                     break;
                 case R.styleable.CustomView_custom_string:
-                    mAttrString = typedArray.getString(R.styleable.CustomView_android_text);
+                    mAttrString = typedArray.getString(R.styleable.CustomView_custom_string);
                     break;
+                case R.styleable.CustomView_android_text:
+                    mAttrAndroidText = typedArray.getString(R.styleable.CustomView_android_text);
                 case R.styleable.CustomView_custom_enum:
                     mAttrEnum = typedArray.getInt(R.styleable.CustomView_custom_enum, 0);
                     break;
@@ -93,15 +109,59 @@ public class CustomView extends ViewGroup {
         }
 
 
-        Logger.i("mAttrInt:%1$d, mAttrBoolean:%2$b, mAttrColor:%3$d, mAttrDimension:%4$d, " +
-                "mAttrFloat: %5$f, mAttrReference:%6$d, mAttrString:%7$s, mAttrEnum:%8$d");
+        Logger.i(String.format("mAttrInt:%1$d, mAttrBoolean:%2$b, mAttrColor:%3$d, mAttrDimension:%4$d, " +
+                "mAttrFloat: %5$f, mAttrReference:%6$d, mAttrString:%7$s, mAttrAndroidText:%8$s, mAttrEnum:%9$d", mAttrInt, mAttrBoolean,mAttrColor,
+                mAttrDimension,mAttrFloat, mAttrReference, mAttrString, mAttrAndroidText, mAttrEnum));
 
         typedArray.recycle();
 
+
+
+        TypedArray globalAttrsTypedArray = context.obtainStyledAttributes(attributeSet, mAttrs);
+        String attrOne = globalAttrsTypedArray.getString(ATTR_INDEX_ONE);
+        int attrTwo = globalAttrsTypedArray.getInt(ATTR_INDEX_TWO, 0);
+
+        Logger.i(String.format("global attr without styleable, attrOne:%1$s, attrTwo:%2$d", attrOne, attrTwo));
+        globalAttrsTypedArray.recycle();
+
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setColor(0xFF000000);
+        mPaint.setTextSize(38);
+        mPaint.setTextAlign(Paint.Align.LEFT);
+
+    }
+
+
+    @Override
+    protected void onMeasure(final int widthMeasureSpec, final int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
 
+    }
+
+    @Override
+    protected void onDraw(final Canvas canvas) {
+        super.onDraw(canvas);
+        // 剧中画字  https://blog.csdn.net/zly921112/article/details/50401976
+        Paint.FontMetrics fm = mPaint.getFontMetrics();
+        String text = "Custom View";
+        //测量文字宽度
+        float textWidth = mPaint.measureText(text);
+        //计算文字基线Y坐标。控件Y中心点加上一段距离就是BaseLineY了，这段距离计算方式为：字体Y中心点减去descent，
+        // 这样就保证了字的Y中心点和控件的Y中心点重合。
+        float textBaseLineY = getHeight()/2 + (fm.descent-fm.ascent)/2  - fm.descent;
+        // textX根据paint的setTextAlign方法的对其方式来决定.
+        float textX = (getWidth() - textWidth) / 2;
+        canvas.drawText(text, textX, textBaseLineY, mPaint);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(final MotionEvent ev) {
+        // TODO 多指触控
+        return super.onInterceptTouchEvent(ev);
     }
 }
