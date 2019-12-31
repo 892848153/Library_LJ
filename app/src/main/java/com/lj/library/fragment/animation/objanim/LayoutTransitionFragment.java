7867d2fc65b1ddcdbe86e1591f5f86e46d2fa25a
jie.liu <892848153@qq.com>
2019-12-31 11:50:03 +0800

    增加LayoutTransition.CHANGING效果的demo

--- app/src/main/java/com/lj/library/fragment/animation/objanim/LayoutTransitionFragment.java
+++ app/src/main/java/com/lj/library/fragment/animation/objanim/LayoutTransitionFragment.java
@@ -7,6 +7,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
@@ -14,6 +15,7 @@ import android.widget.GridLayout;
 
 import com.lj.library.R;
 import com.lj.library.fragment.BaseFragment;
+import com.lj.library.util.AnimatorUtils;
 import com.lj.library.util.LogUtil;
 
 import butterknife.BindView;
@@ -63,6 +65,19 @@ public class LayoutTransitionFragment extends BaseFragment implements CompoundBu
         mTransition.setAnimator(LayoutTransition.APPEARING, (mAppear
                 .isChecked() ? ObjectAnimator.ofFloat(null, "scaleX", 0, 1) : null));
         mGridLayout.setLayoutTransition(mTransition);
+
+        // 开启LayoutTransition.CHANGING动画，due to a layout change not caused by items being added to or removed
+        //  from the container.想要改变layout, 只能想办法改变自己的大小或者位置
+        mViewGroup.getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
+    }
+
+    @OnClick(R.id.changing_btn)
+    public void triggerChangingTransition(Button button) {
+        if (button.getText().equals("CHANGING")) {
+            button.setText("LayoutTransition.CHANGING");
+        } else {
+            button.setText("CHANGING");
+        }
     }
 
     @OnClick(R.id.add_btn)
@@ -84,7 +99,7 @@ public class LayoutTransitionFragment extends BaseFragment implements CompoundBu
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         LogUtil.i(this, "onCheckedChanged");
-        mTransition.setAnimator(LayoutTransition.APPEARING, mAppear.isChecked() ? ObjectAnimator.ofFloat(this, "scaleX", 0, 1) : null);
+        mTransition.setAnimator(LayoutTransition.APPEARING, mAppear.isChecked() ? ObjectAnimator.ofFloat(this, View.SCALE_X.getName(), 0, 1) : null);
         mTransition.setAnimator(LayoutTransition.CHANGE_APPEARING, mChangeAppear.isChecked() ? mTransition.getAnimator(LayoutTransition.CHANGE_APPEARING) : null);
         mTransition.setAnimator(LayoutTransition.DISAPPEARING, mDisAppear.isChecked() ? mTransition.getAnimator(LayoutTransition.DISAPPEARING) : null);
         mTransition.setAnimator(LayoutTransition.CHANGE_DISAPPEARING, mChangeDisAppear.isChecked() ? mTransition.getAnimator(LayoutTransition.CHANGE_DISAPPEARING) : null);
