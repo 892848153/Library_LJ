ac23f93880eaa23189c46082a6f9d2eccd404a22
jie.liu <892848153@qq.com>
2020-01-10 10:39:01 +0800

    add custom transition demo

--- app/src/main/java/com/lj/library/fragment/animation/TransitionAndSceneFragment.kt
+++ app/src/main/java/com/lj/library/fragment/animation/TransitionAndSceneFragment.kt
@@ -1,6 +1,12 @@
 package com.lj.library.fragment.animation
 
+import android.animation.Animator
+import android.animation.ArgbEvaluator
+import android.animation.ValueAnimator
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.transition.*
@@ -13,6 +19,7 @@ import kotlinx.android.synthetic.main.transition_and_scene_fragment.*
  * https://developer.android.google.cn/training/transitions
  * https://developer.android.google.cn/reference/android/transition/Transition.html
  * https://developer.android.google.cn/reference/android/transition/TransitionManager.html
+ * https://developer.android.google.cn/training/transitions/custom-transitions
  *
  * Created by liujie on 2020-01-08.
  */
@@ -94,5 +101,76 @@ class TransitionAndSceneFragment: BaseFragment() {
             val fade = Fade(Fade.IN)
             TransitionManager.beginDelayedTransition(scene_root_2, fade)
             rootView.addView(labelText)
+    }
+
+    /**
+     * https://developer.android.google.cn/training/transitions/custom-transitions
+     * https://github.com/android/animation-samples/blob/master/CustomTransition/Application/src/main/java/com/example/android/customtransition/ChangeColor.java
+     */
+    class CustomTransition: Transition() {
+
+        // Define a key for storing a property value in
+        // TransitionValues.values with the syntax
+        // package_name:transition_class:property_name to avoid collisions
+        private val PROPNAME_BACKGROUND = "com.lj.library.fragment.animation:CustomTransition:background"
+
+        /**
+         * The framework calls this function for every view in the starting scene
+         * contains a reference to the view and a Map instance in which you can store the view values you want.
+         *
+         */
+        override fun captureStartValues(transitionValues: TransitionValues) {
+            // Call the convenience method captureValues
+            captureValues(transitionValues)
+        }
+
+        /**
+         * The framework calls the captureEndValues(TransitionValues) function once for every target view in the ending scene
+         */
+        override fun captureEndValues(transitionValues: TransitionValues) {
+            captureValues(transitionValues)
+        }
+
+        // For the view in transitionValues.view, get the values you
+        // want and put them in transitionValues.values
+        private fun captureValues(transitionValues: TransitionValues) {
+            // Get a reference to the view
+            val view = transitionValues.view
+            // Store its background property in the values map
+            transitionValues.values[PROPNAME_BACKGROUND] = view.background
+        }
+
+        /**
+         * If the starting scene has five targets of which two are removed from the ending scene,
+         * and the ending scene has the three targets from the starting scene plus a new target,
+         * then the framework calls createAnimator() six times:
+         * three of the calls animate the fading out and fading in of the targets that stay in both scene objects;
+         * two more calls animate the fading out of the targets removed from the ending scene;
+         * and one call animates the fading in of the new target in the ending scene.
+         */
+        override fun createAnimator(sceneRoot: ViewGroup, startValues: TransitionValues?, endValues: TransitionValues?): Animator? {
+            if (null == startValues || null == endValues) {
+                return null
+            }
+            val view: View = endValues.view
+            val startBackground: Drawable = startValues.values.get(PROPNAME_BACKGROUND) as Drawable
+            val endBackground: Drawable = endValues.values.get(PROPNAME_BACKGROUND) as Drawable
+
+            if (startBackground is ColorDrawable && endBackground is ColorDrawable) {
+                val startColor: ColorDrawable = startBackground
+                val endColor: ColorDrawable = endBackground
+                if (startColor.color != endColor.color) {
+                    val animator: ValueAnimator = ValueAnimator.ofObject(ArgbEvaluator(), startColor.color, endColor.color)
+                    animator.addUpdateListener {
+                        animator.animatedValue?.let {
+                            view.setBackgroundColor(it as Int)
+                        }
+
+                    }
+                    return animator
+                }
+            }
+            return null
+        }
     }
 }
